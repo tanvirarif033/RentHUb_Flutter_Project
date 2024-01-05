73a67f70fed4a91142a1d63a77f4fb25cbb93090
tanvirarif033 <tanvirarif0802@gmail.com>
2024-01-05 20:10:24 +0600

    update_screen

---
+++ lib/src/features/authentication/screens/update_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class update_screen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Updates',style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+
+}
