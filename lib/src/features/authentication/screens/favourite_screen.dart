57292b129dd4febf20a948b3913809a46425b8de
tanvirarif033 <tanvirarif0802@gmail.com>
2024-01-05 19:47:41 +0600

    favourite_screen

---
+++ lib/src/features/authentication/screens/favourite_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class favourite_screen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     // backgroundColor: Colors.red,
+
+    );
+  }
+
+}
