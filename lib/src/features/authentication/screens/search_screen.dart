9322091f8c5d4a3238decd7982b03eb6699f12f5
tanvirarif033 <tanvirarif0802@gmail.com>
2024-01-05 19:55:42 +0600

    search_screen

---
+++ lib/src/features/authentication/screens/search_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class search_screen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+
+    );
+  }
+
+}
