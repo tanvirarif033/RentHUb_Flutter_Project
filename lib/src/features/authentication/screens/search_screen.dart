52bd0966fb9c06af7e2bcfc120e2cb660d6529b2
Amirul026 <mominamirul96@gmail.com>
2024-01-11 02:04:34 +0600

    searchbar

--- lib/src/features/authentication/screens/search_screen.dart
+++ lib/src/features/authentication/screens/search_screen.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class search_screen extends StatelessWidget{
-  const search_screen({super.key});
+import 'ThemeModeProvider.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      //backgroundColor: Colors.blue,
+  State<SearchScreen> createState() => _SearchScreenState();
+}
 
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final themeModeProvider = Provider.of<ThemeModeProvider>(context);
+    final ThemeMode _currentThemeMode = themeModeProvider.currentThemeMode;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Serach"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SearchAnchor(
+          builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0),
+              ),
+              onTap: () {
+                controller.openView();
+              },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
+              trailing: <Widget>[
+                Tooltip(
+                  message: 'Change brightness mode',
+                  child: IconButton(
+                    isSelected: _currentThemeMode == ThemeMode.dark,
+                    onPressed: () {
+                      themeModeProvider.toggleThemeMode();
+                    },
+                    icon: const Icon(Icons.wb_sunny_outlined),
+                    selectedIcon: const Icon(Icons.brightness_2_outlined),
+                  ),
+                ),
+              ],
+            );
+          },
+          suggestionsBuilder: (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          },
+        ),
+      ),
     );
   }
-
 }
