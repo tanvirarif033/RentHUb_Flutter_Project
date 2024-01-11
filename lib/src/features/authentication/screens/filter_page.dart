8e48f25824ed29996b90d379861f05c1e0a48359
[200~Adnan-Shahriar-1190git config --global user.name <sb.adnan2020@gmail.com>
2024-01-11 08:08:21 +0600

    filter2

--- lib/src/features/authentication/screens/filter_page.dart
+++ lib/src/features/authentication/screens/filter_page.dart
@@ -63,6 +63,63 @@ class _FilterPageState extends State<FilterPage> {
                   ),
                 ),
 
+                SizedBox(height: 20),
+                const Row(
+                  children: [
+                    Icon(Icons.bed_rounded),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Bedrooms',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: ToggleSwitch(
+                    labels: const ['1', '2', '3','4','5','6','7'],
+                    fontSize: 18,
+                    minWidth: 200, // Adjusted minWidth value
+                    minHeight: 60,
+                    activeBgColor: const [Colors.redAccent, Colors.redAccent, Colors.redAccent],
+                    activeFgColor: Colors.black,
+                    inactiveBgColor: Colors.white10,
+                    inactiveFgColor: Colors.black,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                const Row(
+                  children: [
+                    Icon(Icons.bathroom),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Bathrooms',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: ToggleSwitch(
+                    labels: const ['1', '2', '3','4','5','6','7'],
+                    fontSize: 18,
+                    minWidth: 200, // Adjusted minWidth value
+                    minHeight: 60,
+                    activeBgColor: const [Colors.redAccent, Colors.redAccent, Colors.redAccent],
+                    activeFgColor: Colors.black,
+                    inactiveBgColor: Colors.white10,
+                    inactiveFgColor: Colors.black,
+                  ),
+                ),
 
               ],
             ),
